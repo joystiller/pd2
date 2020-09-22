c18385cc48af842fe5e09931ecce7077e2995fba
kaffeocigg <antonsoederberg@gmail.com>
2020-09-22 22:50:11 +0200

    changed start value to mixer

--- my_script.sh
+++ my_script.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -
 echo "hello there"
 echo "starting Pd..."
-pd-l2ork /home/pi/Documents/pd2_repository/SVT.djungle3.pd &
+cd /home/pi/Downloads/pd-0.51-2/bin
+echo pwd
+pkill node
+pd -nogui -rt /home/pi/Documents/pd2_repository/SVT.djungle3.pd &
 sleep 10
 node /home/pi/Documents/pd2_repository/gustavsnode.js
