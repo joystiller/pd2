010ae128a00dbba9bf6cb3ff2da8973a2915378a
kaffeocigg <antonsoederberg@gmail.com>
2020-09-23 15:03:00 +0200

    Added a startShuffler so that the program will never fade in and fade out the same track at startup

--- my_script.sh
+++ my_script.sh
@@ -3,7 +3,7 @@ echo "hello there"
 echo "starting Pd..."
 cd /home/pi/Downloads/pd-0.51-2/bin
 echo pwd
-pkill node
+sudo pkill node
 pd -nogui -rt /home/pi/Documents/pd2_repository/SVT.djungle3.pd &
 sleep 10
 node /home/pi/Documents/pd2_repository/gustavsnode.js
