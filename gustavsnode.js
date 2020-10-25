d89a1091ba69caad27efc19d15fa8a01863b1917
kaffeocigg <antonsoederberg@gmail.com>
2020-10-25 11:00:15 +0100

    Removed slider and changed it to a toggle box.

--- gustavsnode.js
+++ gustavsnode.js
@@ -1,24 +1,34 @@
-var ip = '192.168.1.219';
-var port = '3000';
+// TODO:
+// Is there a way to sort of "run sketch.js" on server startup? Otherwise the user 
+// have to manually hit the refresh button on the html page. 
 
+// Replace slider to another checkbox. 
+
+var ip = '192.168.8.219';
+// var os = require('os')
+// console.log(os.networkInterfaces())
+var port = '3000';
 var express = require('express')
 var app = express()
 var x = 0;
 var dt = new Date();
 dt.setHours(dt.getHours() + 2);
 
+const AbortController = require("abort-controller")
 
 // Gustavs Pd-fetch
 const fetch = require("node-fetch");
+
+
 
 // Set up the server
 var server = app.listen(process.env.PORT || 3000, listen);
 
 // This call back just tells us that the server has started
 function listen() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://' + host + ':' + port);
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listening at http://' + host + ':' + port);
 }
 
 app.use(express.static(__dirname + '/public'));
@@ -29,15 +39,14 @@ var socket = require('socket.io');
 
 var io = socket(server, {
 	handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers);
-        res.end();
-    }
-
+		const headers = {
+			"Access-Control-Allow-Headers": "Content-Type, Authorization",
+			"Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+			"Access-Control-Allow-Credentials": true
+		};
+		res.writeHead(200, headers);
+		res.end();
+	}
 });
 
 
@@ -51,71 +60,181 @@ function newConnection(socket) {
 
 	socket.on('Slider', sliderData);
 
-    // When this user emits, client side: socket.emit('otherevent',some data);
-	async function sliderData(data){ 
+	// When this user emits, client side: socket.emit('otherevent',some data);
+	async function sliderData(data) {
 		//socket.broadcast.emit('mouse', data);
 		console.log(data);
 		console.log(dt);
 
-		x = data.x;	
-		
-		//fetch("http://192.168.1.219:3558", {
-		fetch("http://" + ip + ":" + 3558, {
-			method: "PUT", 
-			body: ";slider1 " + x + ";"
-		}).catch(err => console.error(err));	
+		x = data.x;
+
+
+		// fetch("http://" + ip + ":" + 3558, {
+		// 	method: "PUT", 
+		// 	body: ";slider1 " + x + ";"
+		// }).catch(err => console.error(err));	
 	}
 
 	socket.on('smhiToPd', smhi);
 
-	async function smhi(data){
+	async function smhi(data) {
 		pcat = data.pcat;
 
-		fetch("http://" + ip + ":" + 3558, {
-			method: "PUT", 
-			body: ";pcat " + pcat + ";"
-		}).catch(err => console.error(err));
+		// fetch("http://" + ip + ":" + 3558, {
+		// 	method: "PUT", 
+		// 	body: ";pcat " + pcat + ";"
+		// }).catch(err => console.error(err));
 	}
 
-	socket.on('sunToPd', sunData); 
-	
-	async function sunData(data){
+
+	socket.on('sunToPd', sunData);
+
+	async function sunData(data) {
 		hoursRise = data.hoursRise;
 		minutesRise = data.minutesRise;
 		hoursSet = data.hoursSet;
 		minutesSet = data.minutesSet;
 
+		console.log('sending sundata to pd')
+
 		fetch("http://" + ip + ":" + 3558, {
-			method: "PUT", 
+			method: "PUT",
 			body: ";hoursRise " + hoursRise + "; minutesRise " + minutesRise + "; hoursSet " + hoursSet + "; minutesSet " + minutesSet + ";"
 		}).catch(err => console.error(err));
 	}
+	// require('os');
+	// var os = new 
+
+	// function send2Pd(message = '') {
+	// 	os.system("echo '" + 3 + "' | pdsend 3000 localhost udp");
+	// }
+
 
 
 	socket.on('checkboxToggle', mute);
+	socket.on('musicboxToggle', activateMusic);
 
-	function mute(data) {
-		console.log('return checkbox data: ' + data);
-		console.log(dt);
+	async function mute(data) {
+		if (data) {
+			dt2 = new Date();
+			console.log('Weather installation was unmuted at ' + dt2.toLocaleDateString() + ' ' + dt2.toLocaleTimeString('en-US'));
+		} else {
+			dt2 = new Date();
+			console.log('Weather installation was muted at ' + dt2.toLocaleDateString() + ' ' + dt2.toLocaleTimeString('en-US'));
+		}
 
 		fetch("http://" + ip + ":" + 3558, {
-			method: "PUT", 
-			//body: ";toggle " + 'symbol ' + data + ";"
+			method: "PUT",
 			body: ";toggle " + data + ";"
 		}).catch(err => console.error(err));
 	}
 
-	
+	async function activateMusic(data) {
+		console.log('Music checkbox was set to ' + data + ' at ' + dt);
+
+		fetch("http://" + ip + ":" + 3558, {
+			method: "PUT",
+			body: ";musictoggle " + data + ";"
+		}).catch(err => console.error(err));
+	}
+
+	// }.catch(response => console.error(response));
+
+	// const controller = new AbortController();
+	// const signal = controller.signal;
+
+
+	// const fetchPromise = fetch("http://" + ip + ":" + 3558, {
+	// 	signal,
+	// 	method: "PUT",
+	// 	body: ";toggle " + data + ";"
+	// });
+
+	// timeout(3000, fetch("http://" + ip + ":" + 3558, {
+	// 	signal,
+	// 	method: "PUT",
+	// 	body: ";toggle " + data + ";"
+	// })).then(function (response) {
+	// 	console.log("RESPONSE_FETCHED", response)
+	// }).catch(function (error) {
+	// 	console.log("ERROR_TIME_OUT", error.message);
+	// });
+
+	// closeSockets();
+}
+
+//now we just need to fix this closeSockets-function so that it actually closes the sockets in Pd. is that the main connection above?
+// function closeSockets() {
+// 	socket.on('disconnect', function () {
+// 		console.log("DISCONNECTED_SOCKET!");
+// 		socket.close();
+// 	});
+// }
+
+
+function timeout(ms, promise) {
+	return new Promise(function (resolve, reject) {
+		setTimeout(function () {
+			reject(new Error("timeout"))
+		}, ms)
+		promise.then(resolve, reject)
+	})
 }
 
 
+// var osc = require("osc");
+
+// var getIPAddresses = function () {
+//     var os = require("os"),
+//         interfaces = os.networkInterfaces(),
+//         ipAddresses = [];
+
+//     for (var deviceName in interfaces) {
+//         var addresses = interfaces[deviceName];
+//         for (var i = 0; i < addresses.length; i++) {
+//             var addressInfo = addresses[i];
+//             if (addressInfo.family === "IPv4" && !addressInfo.internal) {
+//                 ipAddresses.push(addressInfo.address);
+//             }
+//         }
+//     }
+
+//     return ipAddresses;
+// };
+
+// var udpPort = new osc.UDPPort({
+//     localAddress: "0.0.0.0",
+//     localPort: 57121
+// });
+
+// udpPort.on("ready", function () {
+//     var ipAddresses = getIPAddresses();
+
+//     console.log("Listening for OSC over UDP.");
+//     ipAddresses.forEach(function (address) {
+//         console.log(" Host:", address + ", Port:", udpPort.options.localPort);
+//     });
+// });
+
+// udpPort.on("message", function (oscMessage) {
+//     console.log(oscMessage);
+// });
+
+// udpPort.on("error", function (err) {
+//     console.log(err);
+// });
+
+// udpPort.open();
+
+// trying to send msg with UDP. Not working... 
+// https://nodejs.org/api/dgram.html
 
 
-
-
-
-
-
-
-
-
+// const dgram = require('dgram');
+// const message = Buffer.from(';Some bytes;');
+// const client = dgram.createSocket('udp4');
+// client.connect(41234, "localhost", (err) => {
+// 	client.send(message, (err) => {
+// 		client.close();
+// 	});
+// });
--- public/sketch.js
+++ public/sketch.js
@@ -1,239 +1,292 @@
-  
-// ITP Networked Media, Fall 2014
-// https://github.com/shiffman/itp-networked-media
-// Daniel Shiffman
+  // ITP Networked Media, Fall 2014
+  // https://github.com/shiffman/itp-networked-media
+  // Daniel Shiffman
 
-var socket;
-var data;
-var weather;
-var api;
-var cityID;
-var key;
-var sliderFX;
-var sliderMX;
-let valMX;
-var toggle;
-const url = '192.168.1.219';
-var Wsymb2;
-var pcat;
+  var socket;
+  var weather;
+  var api;
+  var cityID;
+  var key;
+  var sliderFX;
+  var sliderMX;
+  let valMX;
+  var toggle;
+  var musictoggle;
+  const url = '192.168.8.219';
+  var Wsymb2;
+  var pcat;
+
+  // var os = require('os')
+  // console.log(os.networkInterfaces())
 
 
 
 
-function setup() {
-  noCanvas();
+  function setup() {
+    noCanvas();
 
-  // Start a socket connection to the server
-  socket = io.connect(url + ':3000');
-  
-  const api_url = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.102919/lat/59.336600/data.json';
-  const sun_url = 'https://api.sunrise-sunset.org/json?lat=59.336600&lng=18.102919'
-  
-  async function getSun() {
-    var dtRise = new Date();
-    var dtSet = new Date();
+    //   oscWebSocket = new osc.WebSocketPort({
+    //     url: "172.20.10.2:12345",
+    //     metadata: true
+    //   });
+    //   oscWebSocket.on("ready", onSocketOpen);
+    //   oscWebSocket.on("message", onSocketMessage);
+    // }
 
-    const response = await fetch(sun_url);
-    var hms = await response.json();
+    // function onSendClick() {
+    //   const msg = input.value();
+    //   input.value('');
+    //   // send the OSC message to server. 
+    //   //(osc.js will convert it to binary packet)
+    //   oscWebSocket.send({
+    //     address: "/p5js/sayhi",
+    //     args: [{
+    //       type: "s",
+    //       value: msg
+    //     }]
+    //   });
+    // }
 
-    // Calculations for sunrise
-    var a = hms.results.sunrise.split(':');
-    var b = a[2].split(' ');
+    // Start a socket connection to the server
+    socket = io.connect(url + ':3000');
 
-    var hoursRise = parseInt(a[0]) + 2;
-    var minutesRise = parseInt(a[1]);
-    var secondsRise = parseInt(b[0]);
+    const api_url = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.102919/lat/59.336600/data.json';
+    const sun_url = 'https://api.sunrise-sunset.org/json?lat=59.336600&lng=18.102919'
 
-    if (b[1] == 'PM') {
-      hoursRise = hoursRise + 12;
-    } else if (b[1] == 'AM') {
+    async function getSun() {
+      var dtRise = new Date();
+      var dtSet = new Date();
+
+      const response = await fetch(sun_url);
+      var hms = await response.json();
+
+      // Calculations for sunrise
+      var a = hms.results.sunrise.split(':');
+      var b = a[2].split(' ');
+
+      var hoursRise = parseInt(a[0]) + 2;
+      var minutesRise = parseInt(a[1]);
+      var secondsRise = parseInt(b[0]);
+
+      if (b[1] == 'PM') {
+        hoursRise = hoursRise + 12;
+      } else if (b[1] == 'AM') {}
+
+      dtRise.setHours(hoursRise);
+      dtRise.setMinutes(minutesRise);
+      dtRise.setSeconds(secondsRise);
+
+      console.log('Sun rises at: ' + dtRise);
+
+      // Calculations for sunset
+      var c = hms.results.sunset.split(':');
+      var d = c[2].split(' ');
+
+      var hoursSet = parseInt(c[0]) + 2;
+      var minutesSet = parseInt(c[1]);
+      var secondsSet = parseInt(d[0]);
+
+      if (d[1] == 'PM') {
+        hoursSet = hoursSet + 12;
+      } else if (d[1] == 'AM') {
+        // Do nothing
+      }
+
+      dtSet.setHours(hoursSet);
+      dtSet.setMinutes(minutesSet - 24);
+      // The -24 is 24 minutes from Pd's 2 minutes per fader movement * 12 faders. 
+      // Because of this, The fade will be completed by the time the sun has set.
+      dtSet.setSeconds(secondsSet);
+      console.log('Sun sets at: ' + dtSet);
+
+      var hoursSet = dtSet.getHours();
+      var minutesSet = dtSet.getMinutes();
+
+      var sunData = {
+        hoursRise: hoursRise,
+        minutesRise: minutesRise,
+        hoursSet: hoursSet,
+        minutesSet: minutesSet
+      };
+      socket.emit('sunToPd', sunData);
+
     }
 
-    dtRise.setHours(hoursRise);
-    dtRise.setMinutes(minutesRise);
-    dtRise.setSeconds(secondsRise);
+    async function getISS() {
+      const response = await fetch(api_url);
+      const data = await response.json();
+      console.log(data.timeSeries[2].validTime);
 
-    console.log('Sun rises at: ' + dtRise);
+      var smhiData = {
+        pcat: data.timeSeries[2].parameters[2].level,
+        sunUp: 6,
+        sunDown: 20
+      };
+      //commenting out the socket.emit to Pd because I will change it to 
+      //the radar images. 
+      //socket.emit('smhiToPd', smhiData);
 
-    // Calculations for sunset
-    var c = hms.results.sunset.split(':');
-    var d = c[2].split(' ');
+      Wsymb2 = data.timeSeries[2].parameters[18].values;
+      if (Wsymb2 == 1) {
+        document.getElementById('Wsymb2').innerHTML = 'Clear sky';
+      } else if (Wsymb2 == 2) {
+        document.getElementById('Wsymb2').innerHTML = 'Nearly clear sky';
+      } else if (Wsymb2 == 3) {
+        document.getElementById('Wsymb2').innerHTML = 'Variable cloudiness';
+      } else if (Wsymb2 == 4) {
+        document.getElementById('Wsymb2').innerHTML = 'Halfclear sky';
+      } else if (Wsymb2 == 5) {
+        document.getElementById('Wsymb2').innerHTML = 'Cloudy sky';
+      } else if (Wsymb2 == 6) {
+        document.getElementById('Wsymb2').innerHTML = 'Overcast';
+      } else if (Wsymb2 == 7) {
+        document.getElementById('Wsymb2').innerHTML = 'Fog';
+      } else if (Wsymb2 == 8) {
+        document.getElementById('Wsymb2').innerHTML = 'Light rain showers';
+      } else if (Wsymb2 == 9) {
+        document.getElementById('Wsymb2').innerHTML = 'Moderate rain showers';
+      } else if (Wsymb2 == 10) {
+        document.getElementById('Wsymb2').innerHTML = 'Heavy rain showers';
+      } else if (Wsymb2 == 11) {
+        document.getElementById('Wsymb2').innerHTML = 'Thunderstorm';
+      } else if (Wsymb2 == 12) {
+        document.getElementById('Wsymb2').innerHTML = 'Light sleet showers';
+      } else if (Wsymb2 == 13) {
+        document.getElementById('Wsymb2').innerHTML = 'Moderate sleet showers';
+      } else if (Wsymb2 == 14) {
+        document.getElementById('Wsymb2').innerHTML = 'Heavy sleet showers';
+      } else if (Wsymb2 == 15) {
+        document.getElementById('Wsymb2').innerHTML = 'Light snow showers';
+      } else if (Wsymb2 == 16) {
+        document.getElementById('Wsymb2').innerHTML = 'Moderate snow showers';
+      } else if (Wsymb2 == 17) {
+        document.getElementById('Wsymb2').innerHTML = 'Heavy snow showers';
+      } else if (Wsymb2 == 18) {
+        document.getElementById('Wsymb2').innerHTML = 'Light rain';
+      } else if (Wsymb2 == 19) {
+        document.getElementById('Wsymb2').innerHTML = 'Moderate rain';
+      } else if (Wsymb2 == 20) {
+        document.getElementById('Wsymb2').innerHTML = 'Heavy rain';
+      } else if (Wsymb2 == 21) {
+        document.getElementById('Wsymb2').innerHTML = 'Thunder';
+      } else if (Wsymb2 == 22) {
+        document.getElementById('Wsymb2').innerHTML = 'Light sleet';
+      } else if (Wsymb2 == 23) {
+        document.getElementById('Wsymb2').innerHTML = 'Moderate sleet';
+      } else if (Wsymb2 == 24) {
+        document.getElementById('Wsymb2').innerHTML = 'Heavy sleet';
+      } else if (Wsymb2 == 25) {
+        document.getElementById('Wsymb2').innerHTML = 'Light snowfall';
+      } else if (Wsymb2 == 26) {
+        document.getElementById('Wsymb2').innerHTML = 'Moderate snowfall';
+      } else if (Wsymb2 == 27) {
+        document.getElementById('Wsymb2').innerHTML = 'Heavy snowfall';
+      }
 
-    var hoursSet = parseInt(c[0]) + 2;
-    var minutesSet = parseInt(c[1]);
-    var secondsSet = parseInt(d[0]);
-
-    if (d[1] == 'PM') {
-      hoursSet = hoursSet + 12;
-    } else if (d[1] == 'AM') {
     }
 
-    dtSet.setHours(hoursSet);
-    dtSet.setMinutes(minutesSet);
-    dtSet.setSeconds(secondsSet);
-    console.log('Sun sets at: ' + dtSet);
-
-    
-    var sunData = {
-      hoursRise: hoursRise,
-      minutesRise: minutesRise,
-      hoursSet: hoursSet,
-      minutesSet: minutesSet
-    };
-    socket.emit('sunToPd', sunData); 
-
-  }
-
-  async function getISS() {
-    const response = await fetch(api_url);
-    const data = await response.json();
-    console.log(data.timeSeries[2].validTime);
-
-    var smhiData = {
-      pcat: data.timeSeries[2].parameters[2].level,
-      sunUp: 6,
-      sunDown: 20
-    };
-    socket.emit('smhiToPd', smhiData); 
-
-    Wsymb2 = data.timeSeries[2].parameters[18].values;
-    if (Wsymb2 == 1) {
-      document.getElementById('Wsymb2').innerHTML = 'Clear sky';  
-    } else if (Wsymb2 == 2) {
-      document.getElementById('Wsymb2').innerHTML = 'Nearly clear sky'; 
-    } else if (Wsymb2 == 3) {
-      document.getElementById('Wsymb2').innerHTML = 'Variable cloudiness'; 
-    } else if (Wsymb2 == 4) {
-      document.getElementById('Wsymb2').innerHTML = 'Halfclear sky';   
-    } else if (Wsymb2 == 5) {
-      document.getElementById('Wsymb2').innerHTML = 'Cloudy sky'; 
-    } else if (Wsymb2 == 6) {
-      document.getElementById('Wsymb2').innerHTML = 'Overcast'; 
-    } else if (Wsymb2 == 7) {
-      document.getElementById('Wsymb2').innerHTML = 'Fog';   
-    } else if (Wsymb2 == 8) {
-      document.getElementById('Wsymb2').innerHTML = 'Light rain showers'; 
-    } else if (Wsymb2 == 9) {
-      document.getElementById('Wsymb2').innerHTML = 'Moderate rain showers'; 
-    } else if (Wsymb2 == 10) {
-      document.getElementById('Wsymb2').innerHTML = 'Heavy rain showers';   
-    } else if (Wsymb2 == 11) {
-      document.getElementById('Wsymb2').innerHTML = 'Thunderstorm'; 
-    } else if (Wsymb2 == 12) {
-      document.getElementById('Wsymb2').innerHTML = 'Light sleet showers'; 
-    } else if (Wsymb2 == 13) {
-      document.getElementById('Wsymb2').innerHTML = 'Moderate sleet showers';   
-    } else if (Wsymb2 == 14) {
-      document.getElementById('Wsymb2').innerHTML = 'Heavy sleet showers'; 
-    } else if (Wsymb2 == 15) {
-      document.getElementById('Wsymb2').innerHTML = 'Light snow showers'; 
-    } else if (Wsymb2 == 16) {
-      document.getElementById('Wsymb2').innerHTML = 'Moderate snow showers';   
-    } else if (Wsymb2 == 17) {
-      document.getElementById('Wsymb2').innerHTML = 'Heavy snow showers'; 
-    } else if (Wsymb2 == 18) {
-      document.getElementById('Wsymb2').innerHTML = 'Light rain'; 
-    } else if (Wsymb2 == 19) {
-      document.getElementById('Wsymb2').innerHTML = 'Moderate rain';   
-    } else if (Wsymb2 == 20) {
-      document.getElementById('Wsymb2').innerHTML = 'Heavy rain'; 
-    } else if (Wsymb2 == 21) {
-      document.getElementById('Wsymb2').innerHTML = 'Thunder';   
-    } else if (Wsymb2 == 22) {
-      document.getElementById('Wsymb2').innerHTML = 'Light sleet'; 
-    } else if (Wsymb2 == 23) {
-      document.getElementById('Wsymb2').innerHTML = 'Moderate sleet'; 
-    } else if (Wsymb2 == 24) {
-      document.getElementById('Wsymb2').innerHTML = 'Heavy sleet';   
-    } else if (Wsymb2 == 25) {
-      document.getElementById('Wsymb2').innerHTML = 'Light snowfall'; 
-    } else if (Wsymb2 == 26) {
-      document.getElementById('Wsymb2').innerHTML = 'Moderate snowfall'; 
-    } else if (Wsymb2 == 27) {
-      document.getElementById('Wsymb2').innerHTML = 'Heavy snowfall';   
-    } 
-    
-
-  }
-
-  getISS();
-  getSun();
-  // setInterval(getISS, 60000);
-  setInterval(function () {
     getISS();
     getSun();
-}, 60000);
-  
 
-  angleMode(DEGREES);
-  let d = createDiv();
-  d.style('transform: rotate(' + 90 + 'deg);');
+    //At first, getISS (that is, get the weather) and getSun is called. 
+    //Then, getSun updates once every 24 hours. 
+    //getISS should update once every hour, but it should only send data to Pd
+    //whenever it starts or stops raining.
+
+    setInterval(function () {
+      getSun();
+    }, 86400000);
+
+    setInterval(function () {
+      getISS();
+    }, 3600000)
+
+    angleMode(DEGREES);
+    let d = createDiv();
+    d.style('transform: rotate(' + 90 + 'deg);');
 
 
-  // The hashtag selects the toggle in the html file
-  toggle = select("#toggle");
-  toggle.changed(changeToggle);
-  sliderMX = createSlider(0, 127, 0);
-  sliderMX.input(updateslider);
+    // The hashtag selects the toggle in the html file
+    toggle = select("#toggle");
+    musictoggle = select("#musictoggle");
+    toggle.changed(changeToggle);
+    musictoggle.changed(changeMusicToggle);
+    // sliderMX = createSlider(0, 127, 0);
+    // sliderMX.input(updateslider);
 
-
-
-}
-
-function updateslider() {
-  sendslider(sliderMX.value());
-}
-
-function sendslider(mxval) {
-  console.log("sendslider: " + mxval);
-
-  var data = {
-    x: mxval
-  };
-
-  socket.emit('Slider',data);
-
-}
-
-function changeToggle() {
-  var data = toggle.checked();
-  socket.emit('checkboxToggle', data);
-  if (data){
-    return stopCountdown();
-  } else startCountdown();
-}
-
-function startCountdown() {
-  var counter = 0;
-  var distance = 3600000;
-  document.getElementById("demo").innerHTML = "Commencing countdown...";
-  var i = setInterval(function() { 
-    if (toggle.checked()) {
-      clearInterval(i);
-    } else {
-
-      distance = distance - 1000;
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Display the result in the element with id="demo"
-    document.getElementById("demo").innerHTML = "Muted - Fade in begins in: " + (hours).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + ":"
-    + (minutes).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + ":" + (seconds).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-
-    counter++;
-    if (counter === 3600) {
-      clearInterval(i);
-      stopCountdown();
-    }
   }
-}, 1000);
-}
 
-function stopCountdown () {
-  document.getElementById("toggle").checked = true;
-  socket.emit('checkboxToggle', toggle.checked());
-  document.getElementById("demo").innerHTML = "Click toggle to mute";
+  // function updateslider() {
+  //   sendslider(sliderMX.value());
+  // }
 
-}
+  // function sendslider(mxval) {
+  //   console.log("sendslider: " + mxval);
 
+  //   var data = {
+  //     x: mxval
+  //   };
+
+  //   socket.emit('Slider', data);
+
+  // }
+
+  function changeToggle() {
+    var data = toggle.checked();
+    //console.log("calling the changeToggle function " + data);
+    socket.emit('checkboxToggle', data);
+    if (data) {
+      return stopCountdown();
+    } else startCountdown();
+  }
+
+  function changeMusicToggle() {
+    var data = musictoggle.checked();
+    //console.log("calling the changeMusicToggle function " + data);
+    socket.emit('musicboxToggle', data);
+  }
+
+  function startCountdown() {
+    var counter = 0;
+    var distance = 3600000;
+    document.getElementById("demo").innerHTML = "Commencing countdown...";
+    var i = setInterval(function () {
+      if (toggle.checked()) {
+        clearInterval(i);
+      } else {
+
+        distance = distance - 1000;
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("demo").innerHTML = "Muted - Fade in begins in: " + (hours).toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+            useGrouping: false
+          }) + ":" +
+          (minutes).toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+            useGrouping: false
+          }) + ":" + (seconds).toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+            useGrouping: false
+          });
+
+        counter++;
+        if (counter === 3600) {
+          clearInterval(i);
+          document.getElementById("toggle").checked = true;
+          socket.emit('checkboxToggle', toggle.checked());
+          console.log('this is printed from "if counter === 3600"')
+          document.getElementById("demo").innerHTML = "Click toggle to mute";
+          //stopCountdown();
+        }
+      }
+    }, 1000);
+  }
+
+  function stopCountdown() {
+    //console.log('pringing from stopCountdown() function')
+    document.getElementById("demo").innerHTML = "Click toggle to mute";
+  }
